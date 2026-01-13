44ec386a95e1de37c8ef74d0461155c6bd2a2d49
Ritham Sheti <sethiritham@gmail.com>
2026-01-14 00:20:53 +0530

    feat: Basic chat system in place, server and client can send and recv message independently

--- client.cpp
+++ client.cpp
@@ -2,7 +2,58 @@
 #include <cstring>
 #include <arpa/inet.h>
 #include <unistd.h>
+#include <mutex>
+#include <thread>
 
+std::mutex consoleLock;
+
+void safePrint(const char* msg)
+{
+    std::lock_guard<std::mutex> lock(consoleLock);
+    std::cout << "\r\033[K";
+    std::cout << msg << std::endl;
+    std::cout << "You: " << std::flush;
+}
+
+void sendLoop(int clientSocket)
+{
+    std::string text;
+    while (true)
+    {
+        std::getline(std::cin, text);
+        std::cout << "\033[A\033[K" << std::flush;
+        {
+            std::lock_guard<std::mutex> lock(consoleLock);
+            std::cout << "You: " << text << std::endl;
+        }
+
+        send(clientSocket, text.c_str(), text.size(), 0);
+
+        {
+            std::lock_guard<std::mutex> lock(consoleLock);
+            std::cout << "You: " << text << std::flush;
+        }
+    }
+}
+
+void recieveLoop(int clientSocket)
+{
+    char buffer[1024] = {0};
+    while (true)
+    {
+        memset(buffer, 0, sizeof(buffer));
+        int bytesReceived = recv(clientSocket, buffer, sizeof(buffer), 0);
+        if (bytesReceived <= 0)
+        {
+            break;
+        }
+
+        buffer[bytesReceived] = '\0';
+
+        
+        safePrint(buffer);
+    }
+}
 int main()
 {
     int clientSocket = socket(AF_INET, SOCK_STREAM, 0);
@@ -25,9 +76,10 @@ int main()
 
     std::cout << "Connected to server" << std::endl;
 
-    char buffer[1024] = {0};
-    recv(clientSocket, buffer, sizeof(buffer), 0);
-    std::cout << "Server said: " << buffer << std::endl;
+    std::thread t(recieveLoop, clientSocket);
+    t.detach();
+
+    sendLoop(clientSocket);
 
     
     close(clientSocket);
--- server.cpp
+++ server.cpp
@@ -3,12 +3,64 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <unistd.h>
+#include <mutex>
+#include <thread>
 
+std::mutex consoleLock;
+
+void safePrint(const char* msg)
+{
+    std::lock_guard<std::mutex> lock(consoleLock);
+    std::cout << "\r\033[K";
+    std::cout << msg << std::endl;
+    std::cout << "You: " << std::flush;
+}
+
+void sendLoop(int clientSocket)
+{
+    std::string text;
+    while (true)
+    {
+        std::getline(std::cin, text);
+        std::cout << "\033[A\033[K" << std::flush;
+        {
+            std::lock_guard<std::mutex> lock(consoleLock);
+            std::cout << "You: " << text << std::endl;
+        }
+
+        send(clientSocket, text.c_str(), text.size(), 0);
+
+        {
+            std::lock_guard<std::mutex> lock(consoleLock);
+            std::cout << "You: " << text << std::flush;
+        }
+    }
+}
+
+void recieveLoop(int clientSocket)
+{
+    char buffer[1024] = {0};
+    while (true)
+    {
+        memset(buffer, 0, sizeof(buffer));
+        int bytesReceived = recv(clientSocket, buffer, sizeof(buffer), 0);
+        if (bytesReceived <= 0)
+        {
+            break;
+        }
+
+        buffer[bytesReceived] = '\0';
+
+        
+        safePrint(buffer);
+    }
+}
 int main()
 {
     int serverSocket = socket(AF_INET, SOCK_STREAM, 0);
     if (serverSocket == -1)
     {
+        
         std::cout << "Error creating socket" << std::endl;
         return -1;
     }
@@ -36,8 +88,10 @@ int main()
 
     std::cout << "Client connected" << std::endl;
 
-    const char *message = "Hello from server";
-    send(clientSocket, message, strlen(message), 0);
+    std::thread t(recieveLoop, clientSocket);
+    t.detach();
+
+    sendLoop(clientSocket);
 
     
     close(clientSocket);
